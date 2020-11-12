ec460ff232078584b5864ab3b85530aec357d4d5
vikram <vikram.shinde@morrisonsplc.co.uk>
2020-11-12 14:12:03 +0000

    added sample code

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-//  credentials = file("CREDENTIAL_FILE.json")
-  project     = "terraform-learning-293205"
-  region      = "europe-west2"
+  //  credentials = file("CREDENTIAL_FILE.json")
+  project = "terraform-learning-293205"
+  region  = "europe-west2"
 }
