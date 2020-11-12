972808f155186777943d16390bdc326a5152a46d
vikram <vikram.shinde@morrisonsplc.co.uk>
2020-11-12 16:15:37 +0000

    added more config

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 resource "google_storage_bucket" "s3" {
   name = "testvikramshinde11"
 }
+
+resource "google_storage_bucket" "s32" {
+  name = "testvikramshinde12"
+}
--- providers.tf
+++ providers.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  //  credentials = file("CREDENTIAL_FILE.json")
   project = "terraform-learning-293205"
   region  = "europe-west2"
 }
